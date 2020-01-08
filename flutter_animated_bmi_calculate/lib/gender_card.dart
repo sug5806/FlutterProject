571167b9ea1ce2905f7439341fbff12fc0f23d56
hongdongsu <dsh@storecamera.io>
2020-01-08 23:25:22 +0900

    update gender_card.dart

--- flutter_animated_bmi_calculate/lib/gender_card.dart
+++ flutter_animated_bmi_calculate/lib/gender_card.dart
@@ -1,7 +1,14 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_bmi_calculate/card_title.dart';
 import 'package:flutter_animated_bmi_calculate/gender.dart';
 import 'package:flutter_animated_bmi_calculate/main.dart';
+import 'package:flutter_animated_bmi_calculate/gender_line.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+double _circleSize(BuildContext context) => screenAwareSize(80.0, context);
 
 class GenderCard extends StatefulWidget {
   final Gender initialGender;
@@ -11,8 +18,6 @@ class GenderCard extends StatefulWidget {
   @override
   _GenderCardState createState() => _GenderCardState();
 }
-
-double _circleSize(BuildContext context) => screenAwareSize(80.0, context);
 
 class _GenderCardState extends State<GenderCard> {
   @override
@@ -37,7 +42,7 @@ class _GenderCardState extends State<GenderCard> {
     );
   }
 
-  Widget _drawMainStack(){
+  Widget _drawMainStack() {
     return Stack(
       alignment: Alignment.bottomCenter,
       children: <Widget>[
@@ -67,5 +72,78 @@ class GenderCircle extends StatelessWidget {
         color: Color.fromRGBO(244, 244, 244, 1.0),
       ),
     );
+  }
+}
+
+const double _defaultGenderAngle = math.pi / 4;
+
+const Map<Gender, double> _genderAngles = {
+  Gender.female: -_defaultGenderAngle,
+  Gender.other: 0.0,
+  Gender.male: _defaultGenderAngle,
+};
+
+class GenderIconTranslated extends StatelessWidget {
+  static final Map<Gender, String> _genderImages = {
+    Gender.female: "images/gender_female.svg",
+    Gender.other: "images/gender_other.svg",
+    Gender.male: "images/gender_male.svg",
+  };
+
+  final Gender gender;
+
+  const GenderIconTranslated({Key key, this.gender}) : super(key: key);
+
+  bool get _isOtherGender => gender == Gender.other;
+
+  String get _assetName => _genderImages[gender];
+
+  double _iconSize(BuildContext context) {
+    return screenAwareSize(_isOtherGender ? 22.0 : 16.0, context);
+  }
+
+  double _genderLeftPadding(BuildContext context) {
+    return screenAwareSize(_isOtherGender ? 8.0 : 0.0, context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget icon = Padding(
+      padding: EdgeInsets.only(left: _genderLeftPadding(context)),
+      child: SvgPicture.asset(
+        _assetName,
+        height: _iconSize(context),
+        width: _iconSize(context),
+      ),
+    );
+
+    Widget rotatedIcon = Transform.rotate(
+      angle: -_genderAngles[gender],
+      child: icon,
+    );
+
+    Widget iconWithALine = Padding(
+      padding: EdgeInsets.only(bottom: _circleSize(context) / 2),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          rotatedIcon,
+          GenderLine(),
+        ],
+      ),
+    );
+
+    Widget rotatedIconWithALine = Transform.rotate(
+      alignment: Alignment.bottomCenter,
+      angle: _genderAngles[gender],
+      child: iconWithALine,
+    );
+
+    Widget centeredIconWithALine = Padding(
+      padding: EdgeInsets.only(bottom: _circleSize(context) / 2),
+      child: rotatedIconWithALine,
+    );
+
+    return centeredIconWithALine;
   }
 }
