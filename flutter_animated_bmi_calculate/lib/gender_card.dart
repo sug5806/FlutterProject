e3fac07cf62292480fa33d440cc4cab6e0612f4e
hongdongsu <dsh@storecamera.io>
2020-01-08 23:12:32 +0900

    create basic UI

---
+++ flutter_animated_bmi_calculate/lib/gender_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_bmi_calculate/card_title.dart';
+import 'package:flutter_animated_bmi_calculate/gender.dart';
+import 'package:flutter_animated_bmi_calculate/main.dart';
+
+class GenderCard extends StatefulWidget {
+  final Gender initialGender;
+
+  const GenderCard({Key key, this.initialGender}) : super(key: key);
+
+  @override
+  _GenderCardState createState() => _GenderCardState();
+}
+
+double _circleSize(BuildContext context) => screenAwareSize(80.0, context);
+
+class _GenderCardState extends State<GenderCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.only(top: screenAwareSize(8.0, context)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CardTitle("Gender"),
+              Padding(
+                padding: EdgeInsets.only(top: screenAwareSize(16.0, context)),
+                child: _drawMainStack(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _drawMainStack(){
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        _drawCircleIndicator(),
+      ],
+    );
+  }
+
+  Widget _drawCircleIndicator() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        GenderCircle(),
+      ],
+    );
+  }
+}
+
+class GenderCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: _circleSize(context),
+      height: _circleSize(context),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color.fromRGBO(244, 244, 244, 1.0),
+      ),
+    );
+  }
+}
